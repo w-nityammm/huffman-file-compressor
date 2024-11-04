b864cc2e4a235cb9eca93b5e4c60c5ba54c6be80
Nityam Madaan <130224087+w-nityammm@users.noreply.github.com>
2024-11-04 18:00:41 +0530

    Initial commit

---
+++ use_huffman.py
@@ -0,0 +1,12 @@
+from HuffmanCoding import HuffmanCoding
+import sys
+
+path = "sample.txt"
+
+h = HuffmanCoding(path)
+
+output_path = h.compress()
+print("Compressed file path: " + output_path)
+
+decom_path = h.decompress(output_path)
+print("Decompressed file path: " + decom_path)
